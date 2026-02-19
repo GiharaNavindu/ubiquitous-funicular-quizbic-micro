665a7508e335529b4c85075f92f8641b016d7c09
GiharaNavindu <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2026-02-19 14:38:04 +0530

    add endpints to controller

--- src/main/java/org/gihara/micro/controller/QuestionController.java
+++ src/main/java/org/gihara/micro/controller/QuestionController.java
@@ -3,9 +3,7 @@ package org.gihara.micro.controller;
 import org.gihara.micro.model.Question;
 import org.gihara.micro.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +14,23 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
 
-    @GetMapping("allquestions")
-    public List<Question> getAllQuestions(){
+    // Get all questions
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions() {
+        // Returning the list of questions directly
         return questionService.getAllQuestions();
     }
+    // Get questions by category
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+        // Returning the list of questions by category directly
+        return questionService.getQuestionsByCategory(category);
+    }
+    // Add a new question
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question) {
+        // Returning the success or failure message directly
+        return questionService.addQuestion(question);
+    }
 }
+
