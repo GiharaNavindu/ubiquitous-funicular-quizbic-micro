601a4ca075a55819c4ac57ea117107ac4ff2d138
GiharaNavindu <rathnaweera_pagn_e23@engug.ruh.ac.lk>
2026-02-19 12:09:05 +0530

    project setup

---
+++ src/test/java/org/gihara/micro/MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gihara.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
